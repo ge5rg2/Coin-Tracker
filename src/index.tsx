141dcaafcff0251ce912c61a509248e5d206fa87
ge5rg2 <kghee9612@gmail.com>
2023-01-26 10:48:02 +0900

    #5.0 Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./theme";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
